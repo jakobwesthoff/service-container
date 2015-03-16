b4929fd225d219f1a7b6e504ca78003a214348e5
Jakob Westhoff <jakob@westhoffswelt.de>
2015-03-16 12:46:34 +0100

    Reformatted code

--- ServiceContainer.js
+++ ServiceContainer.js
@@ -11,15 +11,15 @@
  * Construct a Builder object
  */
 module.exports = (function () {
-  var fs, Builder, Container, Definition;
-  fs         = require('fs');
-  Builder    = require('./lib/Builder');
-  Container  = require('./lib/Container');
-  Definition = require('./lib/Definition');
+    var fs, Builder, Container, Definition;
+    fs = require('fs');
+    Builder = require('./lib/Builder');
+    Container = require('./lib/Container');
+    Definition = require('./lib/Definition');
 
-  // Create an instance of the builder with the proper dependencies injected
-  var builder = new Builder(fs, require, Container, Definition);
+    // Create an instance of the builder with the proper dependencies injected
+    var builder = new Builder(fs, require, Container, Definition);
 
-  // Return the initialized builder
-  return builder;
+    // Return the initialized builder
+    return builder;
 }());
