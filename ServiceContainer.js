9f6db37baccc2dd0c228ab226483e8b560c648bf
Jakob Westhoff <jakob@westhoffswelt.de>
2015-03-16 14:49:32 +0100

    Removed fs and require from initial service container

--- ServiceContainer.js
+++ ServiceContainer.js
@@ -2,14 +2,13 @@
  * Construct a Builder object
  */
 module.exports = (function () {
-    var fs, Builder, Container, Definition;
-    fs = require('fs');
+    var Builder, Container, Definition;
     Builder = require('./lib/Builder');
     Container = require('./lib/Container');
     Definition = require('./lib/Definition');
 
     // Create an instance of the builder with the proper dependencies injected
-    var builder = new Builder(fs, require, Container, Definition);
+    var builder = new Builder(Container, Definition);
 
     // Return the initialized builder
     return builder;
